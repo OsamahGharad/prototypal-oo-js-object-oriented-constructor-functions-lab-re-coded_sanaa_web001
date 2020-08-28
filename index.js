cdbcebcc5ef3de5602a29d2fcfd53e943c6576e3
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-28 10:08:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
 function Scooter(year,color,model){
-  // return{
-  //   year,
-  //   color,
-  //   model
-  // }
   this.year=year;
   this.color=color;
   this.model=model;
+}
+
+function driver(name ,age,experience){
+  this.name=name;
+  this.age=age;
+  this.experience=experience;
+}
+
+function PickupLocation(address ,city){
+  this.address=address;
+  this.city=city;
 }
